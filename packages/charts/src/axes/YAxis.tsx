7870a36f1303e83a137eab7158fc62ac9337f8a6
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2019-09-11 17:40:40 +0100

    fix(axis): adding strokeOpacity and missing props
    StrokeOpacity added and used instead of opacity.
    Lots of missing props from axis'.
    
    Closes #29

--- packages/charts/src/axes/YAxis.tsx
+++ packages/charts/src/axes/YAxis.tsx
@@ -1,26 +1,42 @@
 import * as PropTypes from "prop-types";
 import * as React from "react";
+import { strokeDashTypes } from "../utils";
 import Axis from "./Axis";
 
 interface YAxisProps {
     readonly axisAt: number | "left" | "right" | "middle";
-    readonly orient: "left" | "right";
+    readonly className?: string;
+    readonly domainClassName?: string;
+    readonly fill?: string;
     readonly flexTicks?: boolean;
+    readonly fontFamily?: string;
+    readonly fontSize?: number;
+    readonly fontWeight?: number;
+    readonly getMouseDelta?: (startXY: number[], mouseXY: number[]) => number;
     readonly innerTickSize?: number;
+    readonly onContextMenu?: any; // func
+    readonly onDoubleClick?: any; // func
+    readonly orient: "left" | "right";
     readonly outerTickSize?: number;
+    readonly showDomain?: boolean;
+    readonly showTicks?: boolean;
+    readonly showTickLabel?: boolean;
+    readonly stroke?: string;
+    readonly strokeOpacity?: number;
+    readonly strokeWidth?: number;
     readonly tickFormat?: (data: any) => string;
     readonly tickPadding?: number;
     readonly tickSize?: number;
     readonly tickLabelFill?: string;
     readonly ticks?: number;
-    readonly yZoomWidth?: number;
+    readonly tickStroke?: string;
+    readonly tickStrokeOpacity?: number;
+    readonly tickStrokeWidth?: number;
+    readonly tickStrokeDasharray?: strokeDashTypes;
     readonly tickValues?: number[];
-    readonly showTicks?: boolean;
-    readonly className?: string;
+    readonly yZoomWidth?: number;
     readonly zoomEnabled?: boolean;
-    readonly onContextMenu?: any; // func
-    readonly onDoubleClick?: any; // func
-    readonly getMouseDelta?: (startXY: number[], mouseXY: number[]) => number;
+    readonly zoomCursorClassName?: string;
 }
 
 export class YAxis extends React.Component<YAxisProps> {
@@ -36,6 +52,7 @@ export class YAxis extends React.Component<YAxisProps> {
         fill: "none",
         stroke: "#000000",
         strokeWidth: 1,
+        strokeOpacity: 1,
         opacity: 1,
         innerTickSize: 5,
         tickPadding: 6,
@@ -47,6 +64,7 @@ export class YAxis extends React.Component<YAxisProps> {
         fontWeight: 400,
         yZoomWidth: 40,
         zoomEnabled: true,
+        zoomCursorClassName: "react-financial-charts-ns-resize-cursor",
         getMouseDelta: (startXY, mouseXY) => startXY[1] - mouseXY[1],
     };
 
@@ -60,20 +78,25 @@ export class YAxis extends React.Component<YAxisProps> {
 
         const {
             getMouseDelta = YAxis.defaultProps.getMouseDelta,
+            outerTickSize = YAxis.defaultProps.outerTickSize,
+            stroke = YAxis.defaultProps.stroke,
+            strokeWidth = YAxis.defaultProps.strokeWidth,
             ...rest
         } = this.props;
 
-        const { zoomEnabled, ...moreProps } = helper(this.props, this.context);
+        const { zoomEnabled, ...moreProps } = this.helper(this.props, this.context);
 
         return (
             <Axis
                 {...rest}
                 {...moreProps}
-                getMouseDelta={getMouseDelta}
-                zoomEnabled={this.props.zoomEnabled && zoomEnabled}
                 edgeClip
-                axisZoomCallback={this.axisZoomCallback}
-                zoomCursorClassName="react-financial-charts-ns-resize-cursor" />
+                getMouseDelta={getMouseDelta}
+                outerTickSize={outerTickSize}
+                stroke={stroke}
+                strokeWidth={strokeWidth}
+                zoomEnabled={this.props.zoomEnabled && zoomEnabled}
+                axisZoomCallback={this.axisZoomCallback} />
         );
     }
 
@@ -81,50 +104,51 @@ export class YAxis extends React.Component<YAxisProps> {
         const { chartId, yAxisZoom } = this.context;
         yAxisZoom(chartId, newYDomain);
     }
-}
 
-function helper(props: YAxisProps, context) {
-    const {
-        axisAt,
-        yZoomWidth = YAxis.defaultProps.yZoomWidth,
-        orient,
-    } = props;
-    const { chartConfig: { width, height } } = context;
+    private readonly helper = (props: YAxisProps, context) => {
+        const {
+            axisAt,
+            yZoomWidth = YAxis.defaultProps.yZoomWidth,
+            orient,
+        } = props;
+        const { chartConfig: { width, height } } = context;
 
-    let axisLocation;
-    const y = 0;
-    const w = yZoomWidth;
-    const h = height;
+        let axisLocation;
+        const y = 0;
+        const w = yZoomWidth;
+        const h = height;
 
-    if (axisAt === "left") {
-        axisLocation = 0;
-    } else if (axisAt === "right") {
-        axisLocation = width;
-    } else if (axisAt === "middle") {
-        axisLocation = (width) / 2;
-    } else {
-        axisLocation = axisAt;
+        if (axisAt === "left") {
+            axisLocation = 0;
+        } else if (axisAt === "right") {
+            axisLocation = width;
+        } else if (axisAt === "middle") {
+            axisLocation = (width) / 2;
+        } else {
+            axisLocation = axisAt;
+        }
+
+        const x = (orient === "left") ? -yZoomWidth : 0;
+
+        return {
+            transform: [axisLocation, 0],
+            range: [0, height],
+            getScale: this.getYScale,
+            bg: { x, y, h, w },
+            zoomEnabled: context.chartConfig.yPan,
+        };
     }
 
-    const x = (orient === "left") ? -yZoomWidth : 0;
-
-    return {
-        transform: [axisLocation, 0],
-        range: [0, height],
-        getScale: getYScale,
-        bg: { x, y, h, w },
-        zoomEnabled: context.chartConfig.yPan,
-    };
-}
-
-function getYScale(moreProps) {
-    const { yScale: scale, flipYScale, height } = moreProps.chartConfig;
-    if (scale.invert) {
-        const trueRange = flipYScale ? [0, height] : [height, 0];
-        const trueDomain = trueRange.map(scale.invert);
-        return scale.copy()
-            .domain(trueDomain)
-            .range(trueRange);
+    private readonly getYScale = (moreProps) => {
+        const { yScale: scale, flipYScale, height } = moreProps.chartConfig;
+        if (scale.invert) {
+            const trueRange = flipYScale ? [0, height] : [height, 0];
+            const trueDomain = trueRange.map(scale.invert);
+            return scale
+                .copy()
+                .domain(trueDomain)
+                .range(trueRange);
+        }
+        return scale;
     }
-    return scale;
 }

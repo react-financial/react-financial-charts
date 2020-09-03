f1647a7263a387f5286e7dc9ab97ee44f199cc5f
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-09-03 14:02:05 +0100

    fix(series): filtering plotData on AlternateDataSeries
    Filtering the additional data to ones inside the current main plotData.

--- packages/series/src/AlternateDataSeries.tsx
+++ packages/series/src/AlternateDataSeries.tsx
@@ -1,3 +1,4 @@
+import { head, tail } from "@react-financial-charts/core";
 import * as PropTypes from "prop-types";
 import * as React from "react";
 
@@ -6,14 +7,27 @@ export interface AlternateDataSeriesProps<TData> {
 }
 
 export class AlternateDataSeries<TData> extends React.Component<AlternateDataSeriesProps<TData>> {
+    public static contextTypes = {
+        plotData: PropTypes.array,
+        xAccessor: PropTypes.func.isRequired,
+    };
+
     public static childContextTypes = {
         plotData: PropTypes.array,
     };
 
     public getChildContext() {
         const { data } = this.props;
+        const { plotData, xAccessor } = this.context;
+
+        const startDate = xAccessor(head(plotData));
+        const endDate = xAccessor(tail(plotData));
+
         return {
-            plotData: data,
+            plotData: data.filter((d) => {
+                const date = xAccessor(d);
+                return date > startDate && date < endDate;
+            }),
         };
     }
 

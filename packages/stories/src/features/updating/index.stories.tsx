8b39605902cb0ae0db75c55bbd8086bc27a8b6be
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-09-03 17:10:02 +0100

    feat(stories): adding updating data example
    Testing updates with continuous data scale.

---
+++ packages/stories/src/features/updating/index.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Updating } from "./BasicLineSeries";
+
+export default {
+    title: "Features/Updating",
+};
+
+export const continuous = () => <Updating />;

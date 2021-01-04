09958813210358a6f7d0663daaa279b4f9870ae4
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2021-01-04 21:17:56 +0000

    chore(stories): adding annotate example
    Highlighting mondays.
    
    Example for #461

---
+++ packages/stories/src/features/annotated/index.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Annotate } from "@react-financial-charts/annotations";
+import Annotated from "./Annotated";
+
+export default {
+    component: Annotate,
+    title: "Features/Annotate",
+};
+
+export const labels = () => <Annotated />;
